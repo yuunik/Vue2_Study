3fc16d8d49a4976282c5959b0904669c5aa0dd21
zhangyinzhou <zhangyinzhou@zonghenggroup.com.cn>
2023-12-11 17:51:37 +0800

    feat: day5结束。

--- ex_24_vue_router_01/src/main.js
+++ ex_24_vue_router_01/src/main.js
@@ -1,30 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 // 引入路由
-import VueRouter from "vue-router";
-import FindMusic from "@/views/FindMusic.vue";
-import MyMusic from "@/views/MyMusic.vue";
-import MyFriend from "@/views/MyFriend.vue";
-// VueRouter 插件初始化
-Vue.use(VueRouter);
-
-const router = new VueRouter({
-  // 路由规则
-  routes: [
-    {
-      path: "/find",
-      component: FindMusic
-    },
-    {
-      path: "/my",
-      component: MyMusic
-    },
-    {
-      path: "/friend",
-      component: MyFriend
-    }
-  ]
-});
+import router from "@/router";
 
 Vue.config.productionTip = false
 
---
+++ ex_24_vue_router_01/src/router/index.js
@@ -0,0 +1,30 @@
+// 引入 Vue
+import Vue from "vue";
+// 引入路由
+import VueRouter from "vue-router";
+import FindMusic from "@/views/FindMusic.vue";
+import MyMusic from "@/views/MyMusic.vue";
+import MyFriend from "@/views/MyFriend.vue";
+// VueRouter 插件初始化
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    // 路由规则
+    routes: [
+        {
+            path: "/find",
+            component: FindMusic
+        },
+        {
+            path: "/my",
+            component: MyMusic
+        },
+        {
+            path: "/friend",
+            component: MyFriend
+        }
+    ]
+});
+
+// 暴露路由
+export default router;
