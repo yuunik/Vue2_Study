3f488656b43e0a81f691e414217d5176ac1b9b58
zhangyinzhou <zhangyinzhou@zonghenggroup.com.cn>
2023-12-11 18:00:24 +0800

    feat: router-link替换。

--- ex_24_vue_router_01/src/App.vue
+++ ex_24_vue_router_01/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <div class="footer_wrap">
-      <a href="#/find">发现音乐</a>
-      <a href="#/my">我的音乐</a>
-      <a href="#/friend">朋友</a>
+      <router-link to="/find">发现音乐</router-link>
+      <router-link to="/my">我的音乐</router-link>
+      <router-link to="/friend">朋友</router-link>
     </div>
     <div class="top">
       <!-- 路由出口 → 匹配的组件所展示的位置 -->
@@ -39,6 +39,9 @@ body {
   background-color: #333;
   color: #ccc;
   border: 1px solid black;
+}
+.footer_wrap a.router-link-active {
+  background: purple;
 }
 .footer_wrap a:hover {
   background-color: #555;
